b5028f8f3604d4906975dcb3db1a32e6fbcaaa3d
kyle jenne <strongagent1@gmail.com>
2022-11-17 14:52:16 -0500

    Best Buy looking very Dapper for KJ

---
+++ BestBuyBestPractices/DapperProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestBuyBestPractices
+{
+    public class DapperProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public DapperProductRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public void CreateProduct(string name, double price, int categoryID)
+        {
+             _connection.Execute("INSERT INTO PRODUCTS (NAME, PRICE, CATEGORYID) VALUES(@name, @price, @categoryID);",
+            new {name = name, price = price, categoryID = categoryID });
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+           return _connection.Query<Product>("SELECT * FROM products;");
+            
+        }
+        public void UpdateProductName(int productID, string updatedName)
+        {
+            _connection.Execute("UPDATE products SET name = @updatedName WHERE ProductID = @productID;",
+                new {productID = productID, updatedName = updatedName});
+        }
+        public void DeleteProduct(int productID)
+        {
+            _connection.Execute("DELETE FROM reviews WHERE productID = @productID;",
+                new {productID = productID});
+            _connection.Execute("DELETE FROM sales WHERE productID = @productID;",
+                new { productID = productID });
+            _connection.Execute("DELETE FROM products WHERE productID = @productID;",
+                new { productID = productID });
+        }
+    }
+    
+   
+     
+    
+}
--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace BestBuyBestPractices
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace BestBuyBestPractices
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json")
+                            .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+
+            IDbConnection connection = new MySqlConnection(connString);
+
+            var repo = new DapperProductRepository(connection);
+
+            var products = repo.GetAllProducts();
+
+            foreach(var product in products)
+            {
+                Console.WriteLine($"{product.Name} {product.Price} {product.CategoryID}{product.OnSale}");
+                Console.WriteLine();
+            }
+
+            var departmentRepo = new DapperDepartmentRepository(connection);
+
+            var departments = departmentRepo.GetAllDepartments();
+
+            foreach (var department in departments)
+            {
+                Console.WriteLine(department.DepartmentID);
+                Console.WriteLine(department.Name);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
         }
     }
 }
